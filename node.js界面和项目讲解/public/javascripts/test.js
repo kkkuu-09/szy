94c6a4e0f03cb1f29d76019be4d8d65c560daeb2
kkkuu <2696455793@qq.com>
2021-05-17 19:55:35 +0800

    first commit

---
+++ node.js界面和项目讲解/public/javascripts/test.js
@@ -0,0 +1,6 @@
+// document.getElementById("login_button").onclick=function(){
+//     window.location.href='/about';
+// }
+// document.getElementById("rb").onclick=function(){
+//     window.location.href='/register';
+// }
