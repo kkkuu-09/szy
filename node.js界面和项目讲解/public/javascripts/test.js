c145067b50c0f7a436fa1de649cce7ddd57aa638
kkkuu <2696455793@qq.com>
2021-05-28 08:43:17 +0800

    second1.0

--- node.js界面和项目讲解/public/javascripts/test.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("login_button").onclick=function(){
-//     window.location.href='/about';
-// }
-// document.getElementById("rb").onclick=function(){
-//     window.location.href='/register';
-// }
